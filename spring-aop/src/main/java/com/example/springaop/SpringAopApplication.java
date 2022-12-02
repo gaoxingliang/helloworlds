31a9383e4291a4b1b49cdca831a19230c666910a
gaoxingliang <gaoxingliang@outlook.com>
2022-12-02 16:55:22 +0800

    添加示例

--- spring-aop/src/main/java/com/example/springaop/SpringAopApplication.java
+++ spring-aop/src/main/java/com/example/springaop/SpringAopApplication.java
@@ -1,7 +1,7 @@
 package com.example.springaop;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class SpringAopApplication {
