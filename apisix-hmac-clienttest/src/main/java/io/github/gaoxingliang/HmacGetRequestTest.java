19afa24daa53a0fca224c47fe9f5352393355902
gaoxingliang <gaoxingliang@outlook.com>
2024-04-23 14:22:26 +0800

    hmac get & post request test

--- apisix-hmac-clienttest/src/main/java/io/github/gaoxingliang/HmacGetRequestTest.java
+++ apisix-hmac-clienttest/src/main/java/io/github/gaoxingliang/HmacGetRequestTest.java
@@ -27,9 +27,9 @@ public class HmacGetRequestTest {
         String uri = "/enterprises/ent-ba/modules/legalpersoncheck";
         String date = SignUtil.getDate();
         Map<String, Object> qp = new TreeMap<>();
-        qp.put("id", "511111199011110005");
+        qp.put("id", "511111199011111111");
         qp.put("name", "张三");
-        qp.put("phone", "17728897200");
+        qp.put("phone", "15198011111");
         String signature = SignUtil.generateSignature(accessKey, secretKey, "GET", uri, qp, signedHeaders);
 
         String authHeader = String.format("hmac-auth-v1#%s#%s#%s#%s#%s", accessKey, signature, "hmac-sha256", date,
